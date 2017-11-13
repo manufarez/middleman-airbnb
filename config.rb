03d7829070757314ca31292d3b921a88ce352987
Manuel Farez <manufarez@gmail.com>
2017-11-13 17:17:56 +0100

    Added CSS for colors and fonts to over-ride Bootstrap theme

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["papillard", "ssaunier", "monsieurpaillard", "a2bross", "manufarez"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
